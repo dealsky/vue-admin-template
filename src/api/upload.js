073c99bfe33d1bd74c2472942aca5e3e1acf2b58
dealsky <dealsky000@outlook.com>
2018-12-26 13:32:13 +0800

    集成七牛云

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,6 +1,16 @@
 import * as qiniu from 'qiniu-js'
+import request from '@/utils/request'
 
-export const upload = (token, obj, next, error, complete) => {
+const compressType = ['image/png', 'image/jpeg', 'image/bmp', 'image/webp']
+
+export function getUpladToken() {
+  return request({
+    url: '/common/upload/token',
+    type: 'get'
+  })
+}
+
+export function upload(token, obj, next, error, complete) {
   const {
     file
   } = obj
@@ -19,18 +29,21 @@ export const upload = (token, obj, next, error, complete) => {
   }
 
   const options = {
-    quality: 1,
-    noCompressIfLarger: true,
-    maxWidth: 1000,
-    maxHeight: 618
+    quality: 0.92,
+    noCompressIfLarger: true
   }
 
-  qiniu.compressImage(file, options).then(data => {
-    // 调用sdk上传接口获得相应的observable，控制上传和暂停
-    const observable = qiniu.upload(data.dist, key, token, putExtra, config)
+  if (compressType.indexOf(file.type) !== -1) {
+    qiniu.compressImage(file, options).then(data => {
+      // 调用sdk上传接口获得相应的observable，控制上传和暂停
+      const observable = qiniu.upload(data.dist, key, token, putExtra, config)
+      return observable.subscribe(next, error, complete)
+    }).catch(res => {
+      console.log(res)
+      return res
+    })
+  } else {
+    const observable = qiniu.upload(file, key, token, putExtra, config)
     return observable.subscribe(next, error, complete)
-  }).catch(res => {
-    console.log(res)
-    return res
-  })
+  }
 }
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -7,9 +7,8 @@
       <el-form-item label="图片">
         <el-upload
           :show-file-list="false"
-          :on-success="handleUploadSuccess"
-          :before-upload="beforeUpload"
-          :data="dataObj"
+          :http-request="startUpload"
+          :accept="acceptType"
           class="avatar-uploader"
           action="http://upload.qiniup.com">
           <img v-if="form.image" :src="form.image" class="avatar" alt="">
@@ -21,6 +20,8 @@
 </template>
 
 <script>
+import { upload, getUpladToken } from '@/api/upload'
+
 export default {
   data() {
     return {
@@ -28,23 +29,37 @@ export default {
         name: '',
         image: ''
       },
-      dataObj: {
-        token: '4q4D8SH7Xk1Ekrbz8kAoZ6AI2_9zEPxGSwQgR2TF:1uQ3iDe4hVoXrKtC_kdhmGcklcM=:eyJzY29wZSI6Im1lZGlhIiwiZGVhZGxpbmUiOjE1NDU3NTA5NDR9'
-      }
+      acceptType: 'image/png,image/jpeg'
     }
   },
   methods: {
-    beforeUpload() {
+    startUpload(request) {
       this.$message({
         message: '开始上传'
       })
-    },
-    handleUploadSuccess(res) {
-      console.log(res)
-      this.form.image = 'http://pk8vfro30.bkt.clouddn.com/' + res.key
-      this.$message({
-        type: 'success',
-        message: '上传成功'
+
+      getUpladToken().then((res) => {
+        upload(
+          res.data,
+          request,
+          next => {
+            console.log(next)
+          },
+          error => {
+            this.$message({
+              type: 'error',
+              message: '上传失败'
+            })
+            console.log(error)
+          },
+          complete => {
+            this.form.image = 'http://pk8vfro30.bkt.clouddn.com/' + complete.key
+            this.$message({
+              type: 'success',
+              message: '上传成功'
+            })
+          })
+      }).catch(() => {
       })
     }
   }
